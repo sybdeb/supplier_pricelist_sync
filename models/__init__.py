46ffeac4438d85f981c6c929937b09860da28d63
Nerbys <git@nerbys.email>
2025-12-18 16:24:31 +0100

    Add res_partner import to models __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from . import direct_import
 from . import supplier_mapping_template
 from . import product_supplierinfo
 from . import product_template
+from . import res_partner
 from . import import_history
 from . import import_queue
 from . import import_schedule
